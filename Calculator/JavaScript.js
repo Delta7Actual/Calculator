933f741b00a374eed2cac20124fb37ee9ce8a46c
Delta7Actual <142044822+Delta7Actual@users.noreply.github.com>
2023-08-17 13:35:56 +0300

    Initial commit

---
+++ Calculator/JavaScript.js
@@ -0,0 +1,31 @@
+﻿// Get the input element
+const inputElement = document.getElementById("input");
+
+// Handle button clicks
+function handleButtonClick(event) {
+    const clickedButton = event.target;
+    const buttonId = clickedButton.id;
+
+    if (buttonId === 'clear') {
+        inputElement.textContent = '0'; // Clear the input
+    } else if (buttonId === 'equals') {
+        try {
+            const result = eval(inputElement.textContent);
+            inputElement.textContent = result;
+        } catch (error) {
+            inputElement.textContent = 'Error';
+        }
+    } else {
+        if (inputElement.textContent === '0') {
+            inputElement.textContent = clickedButton.textContent;
+        } else {
+            inputElement.textContent += clickedButton.textContent;
+        }
+    }
+}
+
+// Add click event listeners to buttons
+const buttons = document.querySelectorAll(".button, .Sbutton, .done, .clear");
+buttons.forEach(button => {
+    button.addEventListener("click", handleButtonClick);
+});
